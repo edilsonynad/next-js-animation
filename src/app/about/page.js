d6efd74374a0d4d20d2f6ee40e07d0ab26d650da
unknown <edilson.lopes097@outlook.com>
2023-12-29 16:27:07 -0100

    Test the page routes for fun

---
+++ src/app/about/page.js
@@ -0,0 +1,10 @@
+import styles from '../page.module.css'
+export default function About() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <h2>Hello From About</h2>
+      </div>
+    </main>
+  )
+}
